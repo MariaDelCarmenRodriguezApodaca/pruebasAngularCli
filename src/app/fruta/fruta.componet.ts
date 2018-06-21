1a9a8d851acd4197f9de2abeca96afadfcb9cfa4
Maria Del Carmen <mdcra1997@outlook.com>
2018-06-21 00:24:55 -0600

    Aprendido: tipos de datos basicos en TypeScript

--- src/app/fruta/fruta.componet.ts
+++ src/app/fruta/fruta.componet.ts
@@ -4,8 +4,18 @@ import { Component } from '@angular/core';
     selector: 'fruta',
     templateUrl: './fruta.component.html'
 })
+
 export class FrutaComponent{
     //dentro de la clase podemos tener propiedades
     public nombre_componente = 'Componente de frutas';
     public listado_frutas = 'manzana, pera, uva';
+    public nombre:string = "Maria Rodriguez";
+    public edad:number = 21;
+    public mayorDeEdad:boolean = true;
+    public trabajos:Array<string> = ['Carpintero','Albañil','Fontamero'];
+    //any es un tipo de dato cualquiera por ejemplo
+    public cualquiera:Array<any> = [12,true,'Fontamero'];
+    //esta variable puede valer cualquier tipo de dato
+    comodin:any = "cualquier cosa";
+
 }
